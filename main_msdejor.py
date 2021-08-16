433280db9b2076c9fe0576414326975fc1d00a32
msdejor <82190587+msdejor@users.noreply.github.com>
2021-08-16 11:10:14 +0800

    Update main_msdejor.py

--- main_msdejor.py
+++ main_msdejor.py
@@ -9,9 +9,13 @@ import logging
 import random
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import math
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import transforms
+from torch.autograd import Variable
 
 from utils import *
 import time
@@ -19,69 +23,94 @@ from Imagefolder_modified import Imagefolder_modified
 
 # gpu = '2'
 # os.environ['CUDA_VISIBLE_DEVICES'] = gpu
-#    python -u train_pmg_sce_sumloss_decouple_sl.py --gama 0 --bs 30 --gpu 3,4 --net 50 --intro 34card_gama_0 2>&1 | tee bird_decouple_all_gama_0_bs_30_pmgnet50.log
+#    python -u main_msdejor.py --gama 0 --bs 30 --gpu 3,4 --net 50  2>&1 | tee bird_decouple_all_gama_0_bs_30_pmgnet50.log
 import warnings
 warnings.filterwarnings('ignore')
 
-parser = argparse.ArgumentParser(description='Multi-Scale DeJoR')
-parser.add_argument('--bs',  type=int, default=50,help='batch_size')
+parser = argparse.ArgumentParser(description='Multi-Scale DeJOR')
+parser.add_argument('--epochs',  type=int, default=100, help='total_epochs')
+parser.add_argument('--each_class',  default=None, type=int,help='each class samples')
+parser.add_argument('--bs',  type=int, default=30,help='batch_size')
 parser.add_argument('--net',  type=int, default=18,choices=[18,50,16],help='MS resnet18, resnet50 or vgg16')
 parser.add_argument('--data',  type=str, default='bird',help='dataset')
-parser.add_argument('--gama',  type=float, default=2,help='coefficient of negative entropy term : gama')
-parser.add_argument('--lamb', type=float, default=0.1, help ='coefficient of SCE : lambda')
+parser.add_argument('--gama',  type=float, default=2,help='coefficient of negative entropy term : gamma')
+parser.add_argument('--lamb', type=float, default=0.1, help ='coefficient of SKL : lambda')
 parser.add_argument('--gpu', default='1,3', type=str, help='gpu_th')
 parser.add_argument('--gpus', default=None, type=int, help='number of used gpu cards')
-parser.add_argument('--save_dir',  type=str, default='bird' ,help='save dir')
-
+parser.add_argument('--save_dir',  type=str, default='' ,help='save dir')
+parser.add_argument("--es", default=25, help="the start epoch to refurbish ", type=int)
+parser.add_argument("--num_workers", default=4, type=int, help='the number of workers')
+parser.add_argument("--mom", default=0.9, help="momentum", type=float)
 
 args = parser.parse_args()
 gpu = args.gpu
 os.environ['CUDA_VISIBLE_DEVICES'] = gpu
 args.gpus = len(gpu.split(','))
 print(args.gpu, 'total cards:', args.gpus)
-args.save_dir = args.data + '{:.2f}_ms_{}'.format(args.gama,args.net)
+args.save_dir = args.data + '{:.2f}_ms_net{}'.format(args.gama,args.net)+ args.save_dir
 
 
 class MsdejorLoss(nn.Module):
     r"""
     """
-    def __init__(self, Tepoch =10, drop_rate = 0.25, class_num=200):
+    def __init__(self, labels_all, Tepoch =10, drop_rate = 0.25, class_num=200,momentum=0.9,es=25):
         super(MsdejorLoss, self).__init__()
         self.Tepoch = Tepoch
         self.drop_rate = drop_rate
-
         self.class_num = class_num
+        self.soft_labels = torch.zeros(labels_all.shape[0], class_num, dtype=torch.float).cuda(non_blocking=True)
+        self.soft_labels[torch.arange(labels_all.shape[0]), labels_all] = 1
+        self.soft_labels = self.soft_labels.cuda()
+        self.momentum = momentum
+        self.es = es
 
+    def forward(self, logits_1, logits_2, logits_3, logits_4,logits_5, logits_6,logits_7, logits_8,labels, epoch, index):
+        if epoch>=self.es:
+            logits = logits_1 + logits_2 + logits_3 + logits_4 + logits_5 + logits_6 + logits_7 + logits_8 
+            prob = F.softmax(logits.detach(), dim=1)
+            self.soft_labels[index] =  self.momentum * self.soft_labels[index] + (1 - self.momentum)* prob
+            labels = self.soft_labels[index]
+        sloss_sum1, hloss1, js1 = self.loss_sum_calculate(logits_1,logits_2,labels,epoch)
+        w1 = (1-js1).detach()
+        w1 *= w1.shape[0]/w1.sum()
+        sloss_sum2, hloss2, js2 = self.loss_sum_calculate(logits_3,logits_4,labels,epoch)
+        w2 = (1-js2).detach()
+        w2 *= w2.shape[0]/w2.sum()
+        sloss_sum3, hloss3, js3 = self.loss_sum_calculate(logits_5,logits_6,labels,epoch)
+        w3 = (1-js3).detach()
+        w3 *= w3.shape[0]/w3.sum()
+        sloss_sum4, hloss4, js4 = self.loss_sum_calculate(logits_7,logits_8,labels,epoch)
+        w4 = (1-js4).detach()
+        w4 *= w4.shape[0]/w4.sum()
 
-    def forward(self, logits_1, logits_2, logits_3, logits_4,logits_5, logits_6,logits_7, logits_8,labels, epoch):
-
-        sloss_sum1, hloss1 = self.loss_sum_calculate(logits_1,logits_2,labels)
-        sloss_sum2, hloss2 = self.loss_sum_calculate(logits_3,logits_4,labels)
-        sloss_sum3, hloss3 = self.loss_sum_calculate(logits_5,logits_6,labels)
-        sloss_sum4, hloss4 = self.loss_sum_calculate(logits_7,logits_8,labels)
         sloss_sum = sloss_sum1 + sloss_sum2 + sloss_sum3 + 2 * sloss_sum4
-        hloss = hloss1 + hloss2 + hloss3 + 2 * hloss4
+        wsloss_sum = w1*sloss_sum1 + w2*sloss_sum2 + w3*sloss_sum3 + 2 * w4*sloss_sum4
+        # hloss = hloss1 + hloss2 + hloss3 + 2 * hloss4
+        whloss = w1*hloss1 + w2*hloss2 + w3*hloss3 + 2 * w4*hloss4
         ind_sorted = torch.argsort(sloss_sum.data)  # (N) sorted index of the loss
-
         forget_rate = min(epoch, self.Tepoch)/self.Tepoch * self.drop_rate
         num_remember = math.ceil((1 - forget_rate) * logits_1.shape[0])
         ind_update = ind_sorted[:num_remember]  # select the first num_remember low-loss instances
 
-        return sloss_sum[ind_update].mean() - args.gama * hloss.mean()
+        return wsloss_sum[ind_update].mean() - args.gama * whloss.mean()
 
-    def loss_sum_calculate(self,logits_1,logits_2,labels):
+    def loss_sum_calculate(self,logits_1,logits_2,labels,epoch):
         softmax1 = F.softmax(logits_1, dim=1)
         softmax2 = F.softmax(logits_2, dim=1)
-        loss_1 = F.cross_entropy(logits_1, labels, reduction='none')  # (N) loss per instance in this batch
-        loss_2 = F.cross_entropy(logits_2, labels, reduction='none')
-        RCE = torch.sum(-torch.log(softmax1 + 1e-7) * softmax2, dim=-1) + \
-              torch.sum(-torch.log(softmax2 + 1e-7) * softmax1, dim=-1)
+        M = (softmax1+softmax2)/2.
+        if epoch<self.es:
+            loss_1 = F.cross_entropy(logits_1, labels, reduction='none')  # (N) loss per instance in this batch
+            loss_2 = F.cross_entropy(logits_2, labels, reduction='none')
+        else:
+            loss_1 = torch.sum(-F.log_softmax(logits_1, dim=1) * labels, dim=1)
+            loss_2 = torch.sum(-F.log_softmax(logits_2, dim=1) * labels, dim=1)
+        skl = F.kl_div(softmax1.log(), softmax2, reduction='none').sum(1)  + F.kl_div(softmax2.log(), softmax1, reduction='none').sum(1) 
         H = torch.sum(-torch.log(softmax1 + 1e-7) * softmax1, dim=-1) + \
               torch.sum(-torch.log(softmax2 + 1e-7) * softmax2, dim=-1)
+        js = F.kl_div(M.log(), softmax2, reduction='none').sum(1)  + F.kl_div(M.log(), softmax1, reduction='none').sum(1) 
+        sloss_sum = (1-args.lamb) * loss_1 + (1-args.lamb) * loss_2 + args.lamb * skl         #  lambda=0.1
 
-        sloss_sum = (1-args.lamb) * loss_1 + (1-args.lamb) * loss_2 + args.lamb * RCE         #  lambda=0.1
-
-        return sloss_sum, H
+        return sloss_sum, H,  js
 
 def train(nb_epoch, batch_size, store_name, start_epoch=0):
     # setup output
@@ -125,6 +154,9 @@ def train(nb_epoch, batch_size, store_name, start_epoch=0):
     elif args.net == 50:
         net1 = load_ms_layer(model_name='resnet50_ms', classes_nums=len(trainset.classes),pretrain=True, require_grad=True)
         net2 = load_ms_layer(model_name='resnet50_ms', classes_nums=len(trainset.classes),pretrain=True, require_grad=True)
+    elif args.net == 16:
+        net1 = load_ms_layer(model_name='vgg16_ms', classes_nums=len(trainset.classes),pretrain=True, require_grad=True)
+        net2 = load_ms_layer(model_name='vgg16_ms', classes_nums=len(trainset.classes),pretrain=True, require_grad=True)
     if args.gpus > 1:
         net1 = torch.nn.DataParallel(net1)
         net2 = torch.nn.DataParallel(net2)
@@ -132,7 +164,7 @@ def train(nb_epoch, batch_size, store_name, start_epoch=0):
     net1.cuda()
     net2.cuda()
 
-    CoLoss = MsdejorLoss(class_num=len(trainset.classes))
+    CoLoss = MsdejorLoss(labels_all=torch.LongTensor(trainset.targets), Tepoch =10, drop_rate = 0.25, class_num=len(trainset.classes),momentum=args.mom,es = args.es)
     if args.gpus > 1:
         optimizer = optim.SGD([
             {'params': net1.module.classifier_concat.parameters(), 'lr': 0.002},
@@ -209,7 +241,7 @@ def train(nb_epoch, batch_size, store_name, start_epoch=0):
             output_1_1, output_2_1, output_3_1, output_concat_1 = net1(inputs, 4)
             output_1_2, output_2_2, output_3_2, output_concat_2 = net2(inputs, 4)
 
-            loss = CoLoss(output_1_1,output_1_2, output_2_1, output_2_2,output_3_1,output_3_2,output_concat_1,output_concat_2, targets, epoch)                 #   (self, logits, targets, index, epoch, layer_number)
+            loss = CoLoss(output_1_1,output_1_2, output_2_1, output_2_2,output_3_1,output_3_2,output_concat_1,output_concat_2, targets, epoch, index)                 #   (self, logits, targets, index, epoch, layer_number)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -316,13 +348,13 @@ def train(nb_epoch, batch_size, store_name, start_epoch=0):
         file.write('best val acc: {} {}(comb) {}(comb2), com epoch {}, lambda {}  gama {} bs {}'.format(max_val_acc,max_val_acc_com,max_val_acc2,max_com_epoch,args.lamb,args.gama,args.bs))
 
 start_time = time.time()
-train(nb_epoch=100,             # number of epoch
+train(nb_epoch=args.epochs,             # number of epoch
          batch_size=args.bs,         # batch size
          store_name=args.save_dir,     # folder for output
          start_epoch=0,         # the start epoch number when you resume the training
 )       
 
 print('--------------------------------------------')
-print('total time: {}h'.format((time.time()-start_time)//3600))
+print('total time: {:.1f}h'.format((time.time()-start_time)/3600))
 
 
